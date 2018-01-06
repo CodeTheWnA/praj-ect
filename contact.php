8d8c350cf81e8a0c2d66e2577f1b99bdede378fd
CodeTheWnA <pgcoea@gmail.com>
2018-01-06 14:07:36 +0530

    Contact files added

---
+++ contact.php
@@ -0,0 +1,92 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="common/header-footer/header/header.css">
+        <link rel="stylesheet" type="text/css" href="common/header-footer/header/headerResponsive.css">
+        <link rel="stylesheet" type="text/css" href="common/header-footer/footer/footer.css">
+        <link rel="stylesheet" type="text/css" href="common/header-footer/footer/footerResponsive.css">
+        <link rel="stylesheet" type="text/css" href="contact/css/contact.css">
+    </head>
+    <body>
+        <?php include "common/header-footer/header/header.php"; ?>
+        <div class="container">
+        <div class="hexagon">
+                <div class="hexTop"></div>
+                <div class="hexBottom"></div>
+            </div>
+            <div class="small-hex-container">
+                
+            <div class="left">
+                <div class="pair1">
+                    <div class="hexagonS hex1">
+                        <div class="hexTopS"></div>
+                        <div class="hexBottomS"></div>
+                    </div>
+                    <div class="hexagonS hex2">
+                        <div class="hexTopS"></div>
+                        <div class="hexBottomS"></div>
+                    </div>
+                </div>
+                <div class="pair2">
+                    <div class="hexagonS hex3">
+                        <div class="hexTopS"></div>
+                        <div class="hexBottomS"></div>
+                    </div>
+                    <div class="hexagonS">
+                        <div class="hexTopS"></div>
+                        <div class="hexBottomS"></div>
+                    </div>
+                </div>
+                <div class="pair3">
+                    <div class="hexagonS">
+                        <div class="hexTopS"></div>
+                        <div class="hexBottomS"></div>
+                    </div>
+                    <div class="hexagonS">
+                        <div class="hexTopS"></div>
+                        <div class="hexBottomS"></div>
+                    </div> 
+                </div>
+                
+                
+            </div>
+            
+            <div class="right">
+                <div class="pair4">
+                    <div class="hexagonS hex7">
+                        <div class="hexTopS"></div>
+                        <div class="hexBottomS"></div>
+                    </div>
+                     <div class="hexagonS">
+                        <div class="hexTopS"></div>
+                        <div class="hexBottomS"></div>
+                    </div>
+                </div>
+               <div class="pair5">
+                    <div class="hexagonS">
+                        <div class="hexTopS"></div>
+                        <div class="hexBottomS"></div>
+                    </div>
+                    <div class="hexagonS">
+                        <div class="hexTopS"></div>
+                        <div class="hexBottomS"></div>
+                    </div>
+                </div>
+                <div class="pair6">
+                    <div class="hexagonS">
+                        <div class="hexTopS"></div>
+                        <div class="hexBottomS"></div>
+                    </div>
+                    <div class="hexagonS">
+                        <div class="hexTopS"></div>
+                        <div class="hexBottomS"></div>
+                    </div> 
+                </div>
+                
+            </div>
+            </div>
+        
+        </div>
+             
+        <?php include "common/header-footer/footer/footer.php"; ?>
+    </body>
+</html>
